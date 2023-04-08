408b4974d3a132e6c8d59cc237595f2771f653b0
1a11a <86703816+1a11a@users.noreply.github.com>
2023-04-08 09:17:01 +0530

    Customer

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require ("express");
 const mongoose = require ("mongoose");
-const bodyParser = require ("body-parser")
-
+const bodyParser = require ("body-parser");
+const cors = require ('cors')
 const app = express();
 
 const customerRoutes =require('./routes/customers')
 
+//midleware
 app.use(bodyParser.json());
+app.use(cors());
 
 
 app.use(customerRoutes);
